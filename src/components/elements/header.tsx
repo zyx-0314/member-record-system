0755fbb3b99dd5f174f73e9ddd55efbbd65a0528
zyx-0314 <ramirezian037@gmail.com>
2024-09-29 12:42:49 +0800

    refactor(header): Added Variations

--- src/components/elements/header.tsx
+++ src/components/elements/header.tsx
@@ -6,7 +6,32 @@ interface HeaderNavProps {
 }
 
 export default function HeaderNav({ activeNav }: HeaderNavProps) {
+  if (activeNav === "Login") {
+    return LoginHeader();
+  }
+  else {
+    return DefaultHeader(activeNav);
+  }
+};
 
+function LoginHeader() {
+  return (
+    <>
+      <header className="bg-slate-700 flex gap-2 border-solid border-b-2 border-green-300 justify-between items-center px-16 py-4 h-[10dvh]">
+        <a href="/">
+          <Image src={"/aits.png"} width={50} height={50} alt="AITS Logo"></Image>
+        </a>
+        <div className="right flex gap-2">
+          <a href="/">
+            <Button variant={"secondary"} className="rounded-full">Exit</Button>
+          </a>
+        </div>
+      </header>
+    </>
+  );
+}
+
+function DefaultHeader(activeNav: string) {
   return (
     <>
       <header className="bg-slate-700 flex gap-2 border-solid border-b-2 border-green-300 justify-between items-center px-16 py-4 h-[10dvh]">
@@ -15,25 +40,24 @@ export default function HeaderNav({ activeNav }: HeaderNavProps) {
         </a>
         <div className="right flex gap-2">
           <nav className="flex gap-2 ">
-            <a href="/" className={`border-solid border-b-[5px] px-5 py-2 text-white ${activeNav === "Home" ? "border-green-300" : "border-transparent"}`}>
+            <a href="/" className={`border-solid border-green-500 px-5 py-2 text-white ${activeNav === "Home" ? "border-b-[5px]" : ""}`}>
               Home
             </a>
 
-            <a href="/news" className={`border-solid border-b-[5px] px-5 py-2 text-white ${activeNav === "News" ? "border-green-300" : "border-transparent"}`}>
+            <a href="/news" className={`border-solid border-green-500 px-5 py-2 text-white ${activeNav === "News" ? "border-b-[5px]" : ""}`}>
               News
             </a>
 
-            <a href="/about" className={`border-solid border-b-[5px] px-5 py-2 text-white ${activeNav === "About" ? "border-green-300" : "border-transparent"}`}>
+            <a href="/about" className={`border-solid border-green-500 px-5 py-2 text-white ${activeNav === "About" ? "border-b-[5px]" : ""}`}>
               About AITS
             </a>
 
           </nav>
-          {/* <a href="/login" className="rounded-full bg-green-200 px-5 py-2">Login</a> */}
           <a href="/login">
-            <Button>Login</Button>
+            <Button variant={"secondary"} className="rounded-full">Login</Button>
           </a>
         </div>
       </header>
     </>
-  )
-};
+  );
+}
